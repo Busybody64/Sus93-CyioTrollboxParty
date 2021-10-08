f26ecb4a5eb858da4f4b61cb7a9b9c8bb63f24d9
Busybody64 <92069026+Busybody64@users.noreply.github.com>
2021-10-07 20:26:51 -0500

    Create trollbox-party.js
    I can't believe this even worked!

---
+++ trollbox-party.js
@@ -0,0 +1,11 @@
+(async function(){
+  $window({
+    icon: "/c/sys/skins/w93/apps/catExplorer.png",
+    title: "Trollbox (party)",
+    width: 500,
+    height: 470,
+    animationIn: "",
+    animationOut: "hinge",
+    url: "https://trollbox.party"
+  })
+})
